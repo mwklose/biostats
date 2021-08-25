9dabfbabae3914c3e3543e1fed4aed1276bfdc35
Mark Klose <markklose17@gmail.com>
2021-08-25 12:30:20 -0400

    Added directory structure changes

--- biostats/dataset.py
+++ biostats/dataset.py
@@ -1,14 +1,19 @@
 import pandas as pd
 from dataclasses import dataclass
 
+import os
+
 
 @dataclass
 class DataSet:
     values: pd.DataFrame
     description: str
+    length: int
 
     # Open file by filename string
     def __init__(self, file: str, description=None) -> None:
-        with open(file) as f:
+        for f in os.listdir(os.getcwd()):
+            print(f)
+        with open(file, "r") as f:
             self.values = pd.read_csv(f)
         self.description = description
--- test/test.py
+++ test/test.py
@@ -1,10 +1,11 @@
 import unittest
 from biostats.dataset import DataSet
+import os
 
 
 class TestInitialize(unittest.TestCase):
     def test_initialize(self):
-        d1 = DataSet("test.csv", "Initial Dataset")
+        d1 = DataSet(os.path.abspath("files/test.csv"), "Initial Dataset")
 
 
 if __name__ == "__main__":
